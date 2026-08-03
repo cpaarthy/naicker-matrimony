1d647a3ed800feddb231bd5c5d09a4364ccd61d8
cpaarthy <cpaarthy@gmail.com>
2026-08-03 13:56:59 +0530

    use

---
+++ src/utils/downloadJson.js
@@ -0,0 +1,12 @@
+export function downloadJson(filename, dataObj) {
+  const jsonContent = JSON.stringify(dataObj, null, 2);
+  const blob = new Blob([jsonContent], { type: "application/json;charset=utf-8;" });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = filename;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+}
